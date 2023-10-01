b9336ff195063e06c83da29e9992374e22334d9f
Abdessittir <harkati.web.dev@gmail.com>
2023-10-01 12:36:54 +0100

    Implement the pop and shift functionality

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 template <typename T>
 struct Node
@@ -18,12 +19,14 @@ public:
         if (!head)
         {
             head = newNode;
+            ++size;
             return;
         }
 
         if(!tail) {
             tail = newNode;
             head->next = tail;
+            ++size;
             return;
         }
         
@@ -38,6 +41,7 @@ public:
 
         tail->next = newNode;
         tail = newNode;
+        ++size;
     }
 
     void unshift(T value) {
@@ -46,15 +50,55 @@ public:
         if (!head)
         {
             head = newHead;
+            ++size;
             return;
         }
 
         newHead->next = head;
         head = newHead;
+        ++size;
     }
 
     T pop() {
 
+        assert(size > 0);
+
+        if(size == 1) {
+            T value { head->value };
+            head = nullptr;
+            return value;
+        };
+
+        int i{ 0 };
+        Node<T>* current{ head };
+        while (i < size - 2) // the loop should stop one element before the last
+        {
+            current = current->next;
+            ++i;
+        }
+
+        T value { current->next->value };
+        current->next = nullptr;
+        --size;
+
+        return value;
+    }
+
+    void shift() {
+        if(!head) return;
+
+        Node<T>* temp { head };
+
+        if(!tail) {
+            head = tail;
+            delete temp;
+            --size;
+            return;
+        }
+
+        head = head->next;
+
+        delete temp;
     }
 
     T get(int index)
@@ -74,6 +118,7 @@ public:
 private:
     Node<T> *head{};
     Node<T> *tail{};
+    int size { 0 };
 };
 
 int main()
@@ -95,5 +140,14 @@ int main()
         std::cout << list.get(i) << ' ';
     }
     std::cout << '\n';
+
+    std::cout << list.pop() << '\n';
+    list.shift();
+
+    for(int i{ 0 }; i < 13; ++i) {
+        std::cout << list.get(i) << ' ';
+    }
+    std::cout << '\n';
+
     return 0;
 }
