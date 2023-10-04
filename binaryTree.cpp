8878d6f5de34ac7703a3bbc44acd055b246704f3
Abdessittir <abdessittir@gmail.com>
2023-10-04 17:08:12 +0100

    Start implementing binaryTree

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,7 +1,19 @@
+#include <string>
+
 template<typename T>
 struct Node
 {
     T value{};
+    std::string name{};
     Node<T>* left{ nullptr };
     Node<T>* right{nullptr};
+};
+
+template<typename T>
+class BinaryTree
+{
+public:
+    void insertNode(){}
+private:
+    Node<T>* root{ nullptr };
 };
